89258bafd9ec138e61b527b0e66b8325d21f2afc
dashcraft <ashcraftbookkeeping@gmail.com>
2017-01-06 20:03:32 -0600

    Added actual routes, the authenticationcontroller export was never defined in authentication.js, i added some basic routes, now it is firing, just need to implement logic

---
+++ controllers/authentication.js
@@ -0,0 +1,14 @@
+
+
+
+
+exports.login = function(req,res,next){
+	console.log('woohooo i exist now');
+}
+
+
+exports.register = function(req,res,next){
+	console.log('woohooo i exist now');
+}
+
+
