de3048d03d2020ca31a7d23e78f2e0f660396e84
RawleJuglal <rawledjuglal@gmail.com>
2017-01-07 00:08:52 +0000

    core(energy_news): creating jwt token auth with passport

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var mongoose = require('mongoose');
+var config = require('./config/main');
 
 var app = express();
+
+// Database Connection
+mongoose.connect(config.database); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,13 +19,25 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
+// Enable CORS from client-side
+app.use(function(req, res, next) {  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+const router = require('./router');  
+
+router(app); 
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
